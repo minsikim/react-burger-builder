d5149ead5a75484dd164dd342e57363741328f3c
Salinger MS <minsiua@gmail.com>
2018-08-21 05:30:31 +0900

    Initial commit

---
+++ src/BasicComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+const basic = (props) => (
+    <Aux>
+        <div></div>
+    </Aux>
+)
+
+export default basic;
---
+++ src/BasicContainer.js
@@ -0,0 +1,15 @@
+import React, {Comoponent} from 'react';
+import Aux from '../../hoc/Aux';
+
+class Basic extends Component {
+    render () {
+        return (
+            <Aux>
+                <div></div>
+                <div></div>
+            </Aux>
+        )
+    }
+}
+
+export default Basic;
