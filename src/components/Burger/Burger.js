f3286beeeb5c9629b04b5c16e56073ff937190d0
Salinger MS <minsiua@gmail.com>
2018-08-21 06:12:28 +0900

    adding funtions to build control(not finished)

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,16 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map((igKey) => {
         return [...Array(props.ingredients[igKey])].map((_, i)=>(
             <BurgerIngredient key={igKey + i} type={igKey}/>
         ))
-    })
-
+    }).reduce((prevValue, currentValue)=>{
+        return prevValue.concat(currentValue)
+    }, []);
+    if (transformedIngredients.length === 0) transformedIngredients = <p>Please start adding ingredients</p>
+    console.log(transformedIngredients);
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
